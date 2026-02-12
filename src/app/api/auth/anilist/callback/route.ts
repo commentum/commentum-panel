f422d459da0a652eb9cab20b8df3889619293d22
Roshan <roshan.codespace@gmail.com>
2026-02-13 02:56:21 +0530

    remove redirect uri properly

--- src/app/api/auth/anilist/callback/route.ts
+++ src/app/api/auth/anilist/callback/route.ts
@@ -31,7 +31,6 @@ export async function GET(request: NextRequest) {
         client_id: CONFIG.ANILIST_ID,
         client_secret: CONFIG.ANILIST_SECRET,
         code,
-        redirect_uri: CONFIG.REDIRECT_URI,
       }),
     });
 
